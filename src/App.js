32b41671e69deda328233f02c961cdba5416101d
zeynepaytekin <152427296+zeynepaytekin@users.noreply.github.com>
2025-06-02 04:00:57 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+//import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Detail from './pages/Detail';
 import NotFound from './pages/NotFound';
